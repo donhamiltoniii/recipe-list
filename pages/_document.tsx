90020a8ce4ec13f0beccd4489fd2d5e2f2c57a2b
Don Hamilton III <donhamiltoniii@gmail.com>
2022-02-03 11:12:15 -0500

    Update google font imports

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,15 +1,26 @@
-import Document, { Html, Head, Main, NextScript } from 'next/document'
+import Document, { Html, Head, Main, NextScript } from 'next/document';
 
 export default class MyDocument extends Document {
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <link rel="preconnect" href="https://fonts.googleapis.com" />
+          <link
+            rel="preconnect"
+            href="https://fonts.gstatic.com"
+            crossOrigin="anonymous"
+          />
+          <link
+            href="https://fonts.googleapis.com/css2?family=Open+Sans&family=Permanent+Marker&display=swap"
+            rel="stylesheet"
+          />
+        </Head>
         <body>
           <Main />
           <NextScript />
         </body>
       </Html>
-    )
+    );
   }
 }
