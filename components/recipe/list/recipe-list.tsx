e889223f38763a4e4c632ed3aba024c0bed22c07
Don Hamilton III <donhamiltoniii@gmail.com>
2022-03-17 22:31:26 -0400

    Remove all references to RecipeCardProps

--- components/recipe/list/recipe-list.tsx
+++ components/recipe/list/recipe-list.tsx
@@ -2,19 +2,19 @@
 import { RecipeCard } from '../card/recipe-card';
 
 // Types
-import { RecipeCardProps } from '../../../types/recipe';
+import { RecipeProps } from '../../../types/recipe';
 
 // Styles
 import styles from './recipe-list.module.scss';
 
 interface RecipeListProps {
-  allRecipes: RecipeCardProps[];
+  allRecipes: RecipeProps[];
 }
 
 export const RecipeList = ({ allRecipes }: RecipeListProps) => {
   return (
     <ul className={styles['recipe-list']}>
-      {allRecipes.map(({ slug, title }: RecipeCardProps) => (
+      {allRecipes.map(({ slug, title }: RecipeProps) => (
         <li className={styles['recipe-list__item']} key={slug}>
           <RecipeCard slug={slug} title={title} />
         </li>
