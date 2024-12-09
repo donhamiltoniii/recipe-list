1c5a53bbe05fb62b1286f813217d1dd707432881
Don Hamilton III <donhamiltoniii@gmail.com>
2024-12-09 10:22:33 -0500

    feat: update markdown processor
    - replace remark with `unified` package
    - add new processing rules for markdown files

--- lib/markdownToHtml.ts
+++ lib/markdownToHtml.ts
@@ -1,8 +1,15 @@
-import remark from 'remark';
-import html from 'remark-html';
+import rehypeStringify from 'rehype-stringify';
 import remarkGfm from 'remark-gfm';
+import remarkParse from 'remark-parse';
+import remarkRehype from 'remark-rehype';
+import { unified } from 'unified';
 
 export default async function markdownToHtml(markdown: string) {
-  const result = await remark().use(remarkGfm).use(html).process(markdown);
+  const result = await unified()
+    .use(remarkParse) // Parse markdown.
+    .use(remarkGfm) // Support GFM (tables, autolinks, tasklists, strikethrough).
+    .use(remarkRehype) // Turn it into HTML.
+    .use(rehypeStringify) // Serialize HTML.
+    .process(markdown);
   return result.toString();
 }
